bac1a77e703325bf60d9017bc1fcbb5ceefbc3aa
Jannii <Johancn83@gmail.com>
2017-02-23 12:36:47 +0100

    emailskit

--- Email.aspx.cs
+++ Email.aspx.cs
@@ -9,14 +9,15 @@ using System.Web.UI.WebControls;
 
 public partial class Email : System.Web.UI.Page
 {
+    Customers c = new Customers();
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        c = (Customers)Session["myCustomer"];
     }
 
     protected void Button1_Click(object sender, EventArgs e)
     {
-        MailMessage o = new MailMessage("iuliunicolaebarcan@gmail.com", "iuliunicolaebarcan@gmail.com", "grattis", "you have a new acount");
+        MailMessage o = new MailMessage("iuliunicolaebarcan@gmail.com", c.Email, "grattis", "you have a new acount");
         NetworkCredential netCred = new NetworkCredential("iuliunicolaebarcan@gmail.com", "tarabostes");
         SmtpClient smtpobj = new SmtpClient("smtp.gmail.com", 587);
         smtpobj.EnableSsl = true;
