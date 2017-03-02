e3ed410f38d80dcc9fc1f16e14c052d66c70e801
Jannii <Johancn83@gmail.com>
2017-03-02 10:22:20 +0100

    rensat i master

--- MasterPage.master.cs
+++ MasterPage.master.cs
@@ -20,20 +20,14 @@ public partial class MasterPage : System.Web.UI.MasterPage
     
     string totalPrice = "";
     static Random rnd;
-    //  List<Books> myBooks;
-    // Books book;
 
     Customers actualCustomer;
     Administrator actualAdmin;
     protected void Page_Load(object sender, EventArgs e)
     {
-        //    myBooks = new List<Books>();
-        // myBooks = (List<Books>)Session["myBooks"];
-        //labelPrice.Text = this.getPrice(myBooks);
         if (!IsPostBack)
         {
-
-            // book = new Books();
+            
             actualCustomer = new Customers();
             
 
@@ -58,46 +52,11 @@ public partial class MasterPage : System.Web.UI.MasterPage
 
             labelName.Text = name;
 
-            /* if (myBooks.Count > 0)
-             {
-                 linckOut.Visible = true;
-                 ddLstBks.Visible = true;
-
-                 ddLstBks.DataTextField = "title";
-                 ddLstBks.DataValueField = "isbn";
-                 ddLstBks.DataSource = myBooks;
-                 ddLstBks.DataBind();
-              //   getPrice(myBooks);
-
-             }
-             else {
-                 linckOut.Visible = false;
-                 ddLstBks.Visible = false;
-
-             }*/
-
-            //   book = getAllBooks();
-
-            //   LinkButton2.Text = book.Title;
-            //   imageBook.ImageUrl = "~/Styles/" + book.Isbn + ".gif";
-            // Session["bookAdd"] = book;
+           
 
         }
     }
-    protected void Button1_Click(object sender, EventArgs e)
-    {
-
-    }
-
-    protected void Button8_Click(object sender, EventArgs e)
-    {
-
-    }
-
-    protected void TextBox1_TextChanged(object sender, EventArgs e)
-    {
-
-    }
+   
 
     protected void Button8_Click1(object sender, EventArgs e)
     {
@@ -190,22 +149,6 @@ public partial class MasterPage : System.Web.UI.MasterPage
     }
    
 
-    protected void ddLstBks_SelectedIndexChanged(object sender, EventArgs e)
-    {
-    }
-
-    protected void Button9_Click(object sender, EventArgs e)
-    {
-        /* int id = ddLstBks.SelectedIndex;
-         string removedIsbn = myBooks[id].Isbn;
-         string allQuantity = getAllQuantity(removedIsbn);
-         int newQuantity = (Convert.ToInt32(myBooks[id].Quantity) + Convert.ToInt32(allQuantity));
-         updateQuantity(newQuantity, removedIsbn);
-         ddLstBks.Items.RemoveAt(id);
-          myBooks.RemoveAt(id);      
-         Session["myBooks"] = myBooks;
-         Response.Redirect("BookPage.aspx");*/
-    }
 
     protected void LinkButton1_Click(object sender, EventArgs e)
     {
@@ -256,52 +199,6 @@ public partial class MasterPage : System.Web.UI.MasterPage
         Response.Redirect("Default.aspx");
     }
 
-    protected void XmlDataSource1_Transforming(object sender, EventArgs e)
-    {
-
-    }
-
-    protected void XmlDataSource2_Transforming(object sender, EventArgs e)
-    {
-
-    }
-    protected void getAllBooks()
-    {
-        /*   it was BOOKS   Books addBook = new Books();
-          List<Books> lib = new List<Books>();
-          rnd = new Random();
-          String connString = System.Configuration.ConfigurationManager.ConnectionStrings["WebbAppConnString"].ToString();
-          conn = new MySql.Data.MySqlClient.MySqlConnection(connString);
-          conn.Open();
-          queryStr = "";
-          queryStr = "Select * from (SELECT * FROM books ORDER BY date DESC limit 0,3) as alias ORDER BY date desc;";
-          cmd = new MySql.Data.MySqlClient.MySqlCommand(queryStr, conn);
-          reader = cmd.ExecuteReader();
-          while (reader.Read())
-          {
-               string title = reader.GetString(reader.GetOrdinal("title"));
-              string  isbn = reader.GetString(reader.GetOrdinal("isbn"));
-              lib.Add(new Books(title, isbn));
-
-          }
-          conn.Close();
-          int r = rnd.Next(lib.Count);
-          addBook = lib[r];
-
-
-          return addBook;*/
-    }
-
-
-    protected void LinkButton2_Click(object sender, EventArgs e)
-    {
-
-        /* Books bookAdd = (Books)Session["bookAdd"];
-          Session["ISBN"] = bookAdd.Isbn;
-        //  LinkButton2.Text = bookAdd.Title;
-
-          Response.Redirect("SelectedProduct.aspx");*/
-    }
 
     protected void linkBooks_Click(object sender, EventArgs e)
     {
@@ -349,16 +246,7 @@ public partial class MasterPage : System.Web.UI.MasterPage
     {
         double totalPrice = 0;
         string price = "0";
-
-        /* for (int i = 0; i < b.Count; i++)
-         {
-             string tempPrice = b[i].Price;
-             double tempPrice1 = Convert.ToDouble(tempPrice, CultureInfo.InvariantCulture);
-             totalPrice = totalPrice + tempPrice1;
-         }
-         price = totalPrice + "";*/
-
-
+        
 
         return price;
     }
@@ -366,36 +254,9 @@ public partial class MasterPage : System.Web.UI.MasterPage
     {
         string q = "0";
 
-        /* String connString = System.Configuration.ConfigurationManager.ConnectionStrings["WebbAppConnString"].ToString();
-         conn = new MySql.Data.MySqlClient.MySqlConnection(connString);
-         conn.Open();
-         queryStr = "";
-         queryStr = "SELECT * from books where isbn= '" + s + "'";
-         cmd = new MySql.Data.MySqlClient.MySqlCommand(queryStr, conn);
-         reader = cmd.ExecuteReader();
-         while (reader.Read())
-         {
-             q = reader.GetString(reader.GetOrdinal("quantity"));
-         }
-         conn.Close();*/
+        
         return q;
     }
-    protected void updateQuantity(int q, string isbn)
-    {
-
-        /*  String connString = System.Configuration.ConfigurationManager.ConnectionStrings["WebbAppConnString"].ToString();
-          conn = new MySql.Data.MySqlClient.MySqlConnection(connString);
-          conn.Open();
-          queryStr = "";
-          queryStr = "update  books set quantity = '" + q + "' where isbn = '" + isbn + "'";
-          cmd = new MySql.Data.MySqlClient.MySqlCommand(queryStr, conn);
-          cmd.ExecuteReader();
-          conn.Close();*/
-
-    }
-
-
-
     protected void LinkButton8_Click1(object sender, EventArgs e)
     {
         actualCustomer = (Customers)Session["myCustomer"];
